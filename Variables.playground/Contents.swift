63d2c9c7417b3d2cd6bdb854f1788db023e2a762
Meghan McCandless <meghanmccandless@gmail.com>
2016-08-20 16:01:56 -0400

    added favorite character

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,7 +4,7 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Lyanna Mormont"
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
