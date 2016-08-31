c95a756f4fe47423acc5cc0cc12c1cabde40a18e
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-08-31 12:46:34 -0700

    Variables ??

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,7 +4,7 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Petyr Baelish"
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
