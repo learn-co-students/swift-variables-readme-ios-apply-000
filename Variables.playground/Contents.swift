bf70da8ceece033daf12d075a67c9d5d7a732427
Jonathan Shitrit <jonathanshitrit@Jonathans-MBP.fios-router.home>
2016-12-12 22:10:19 -0500

    Fixed errors in playgroundt

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,9 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Bran Stark"
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+ultimateFavoriteCharacter != "Daenerys Targaryen"
