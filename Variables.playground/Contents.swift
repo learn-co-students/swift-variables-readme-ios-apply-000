69cdb24ef590d6a93c54677f0db91bfb7191681b
William Leahy <Will@Williams-MacBook-Pro.local>
2016-12-16 16:23:04 -0500

    Complete problems

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,11 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Jon Snow"
+print(favoriteCharacter)
 
-let ultimateFavoriteCharacter = "Arya Stark"
+var ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
 ultimateFavoriteCharacter = "Daenerys Targaryen"
+print(ultimateFavoriteCharacter)
