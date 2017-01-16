6d281681c0498311edfd736de69a72b81707ef2d
mikeydoe <Michael@Michaels-MacBook-Pro.local>
2017-01-16 14:35:16 -0500

    finished lab

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,23 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Bill"
 
-let ultimateFavoriteCharacter = "Arya Stark"
+var ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
 ultimateFavoriteCharacter = "Daenerys Targaryen"
+
+let question = "Where is your home"
+
+var answer = "New York"
+
+print(question)
+print(answer)
+
+let bae = "Rebecca"
+
+let notbae: String = "Rebecca"
+
+print(bae)
+print(notbae)
