e21aeae6765d689b7c7ad48b8dbd5ac36dfe4251
burakflatiron <burakflatiron@Tech-Book.home>
2017-02-05 10:47:43 -0500

    Completed Variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,12 +1,44 @@
+var nameOfPerson = "Burak"
+print(nameOfPerson)
+
+nameOfPerson = " Jason Burak "
+
 var favoriteCharacter = "Jon Snow"
 print(favoriteCharacter)
 
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+//favoriteCharacter = 76
 
-let ultimateFavoriteCharacter = "Arya Stark"
+let ultimateFavoriteCharacter = "Arya " + nameOfPerson + " Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+//ultimateFavoriteCharacter = "Daenerys Targaryen"
+
+let favoriteColor = "Blue"
+print (favoriteColor)
+
+
+
+
+
+// Type Annotations
+
+
+let question = "What's your favorite food ?"
+print(question)
+
+var answer:String = "Pasta" //explicit
+print(answer)
+
+
+// Naming Constants and Variables 
+
+let favoriteArtist = "Bob Ross"
+let leastFavoriteArtist: String = "Not Bob Ross"
+
+//let favoriteArtist = "BLAH"
+
+print(favoriteArtist)
+print(leastFavoriteArtist)
