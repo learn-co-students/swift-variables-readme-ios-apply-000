47981d293b0f003e0c54c446b45df93c7e8933f4
cmm0217 <cmm0217@gmail.com>
2017-03-25 11:14:27 -0400

    Complete

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,7 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Bill"
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
-
-ultimateFavoriteCharacter = "Daenerys Targaryen"
