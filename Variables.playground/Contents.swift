b76be7dbcac86e8e564f62b1541701d2729b9917
Mike Kamp <michael.kampouris@gmail.com>
2017-04-06 00:56:53 -0400

    updated lab

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,4 +1,4 @@
-var favoriteCharacter = "Jon Snow"
+/*var favoriteCharacter = "Jon Snow"
 print(favoriteCharacter)
 
 favoriteCharacter = "Tyrion Lannister"
@@ -10,3 +10,20 @@ let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
 ultimateFavoriteCharacter = "Daenerys Targaryen"
+ */
+
+let myName = "Michael Kampouris"
+var myAge = 28
+var currentMood = "Excited"
+
+currentMood = "sleepy"
+
+var myStatement = "\(myName), which is \(myAge) years old, is \(currentMood)"
+
+
+print (myStatement)
+
+// meName = "John Doe" - This would throw an error because my name is an immutable value.
+
+// myName should be a constant because my name doesnt change.
+// myAge should be a variable because it changes every year.
