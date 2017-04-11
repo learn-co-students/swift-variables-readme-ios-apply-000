4613ad174e41efe241df31e116d318385714c020
“Diljeet <jeetds1994@gmail.com>
2017-04-11 19:43:07 -0400

    added comment for isses

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,10 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
-
+//favoriteCharacter = 76
+//cannot change as favoriteCharacter is defineed as of type String not Int
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+//ultimateFavoriteCharacter = "Daenerys Targaryen"
+//cannot change as ulimtateCharcater is a let not var
